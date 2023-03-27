8a37f87a9673589d066b8e1ab5be35cb44da43e5
Wieland Wüller <wielandfriedrichwuller@gmail.com>
2023-03-27 15:55:46 +0200

    Improved GET InputFields

--- requests/GET/CheckForInputFields.php
+++ requests/GET/CheckForInputFields.php
@@ -7,9 +7,10 @@ if(empty($_GET['action'])) {
 
 $pdo = new PDO("mysql:host=localhost;dbname=jrk", "root", "");
 
-$stmt = $pdo->prepare("SELECT a.name AS action_name, a.min_needed_helper, a.running, f.inputfield, f.field_description FROM actions AS a
+$stmt = $pdo->prepare("SELECT a.name AS action_name, a.min_needed_helper, a.running, f.inputfield, f.field_description, f.input_requirements, vt.name AS value_type_name FROM actions AS a
                     LEFT JOIN fields AS f ON a.id = f.action_id
-                    LEFT JOIN input_types AS it ON f.inputfield = it.id
+                    LEFT JOIN input_types AS it ON f.inputfield = it.id      
+                    LEFT JOIN value_types AS vt ON f.value_type = vt.id
                     WHERE a.id = ? AND a.running = ?");
 
 $stmt->bindValue(1, $_GET['action']);
@@ -20,13 +21,29 @@ $stmt->execute();
 $actions = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $actionInformation = ["action_name" => $actions[0]['action_name'], "min_needed_helper" => $actions[0]['min_needed_helper']];
 
+$currentCount = 1;
+$numItems = 0;
+
 foreach ($actions as $key => $action) {
-    $actionInformation['formular'][] = '
+    $numItems = count($actions);
+    $actionInformation[$key]["input"]["formular"] = '
         <label for="'.$action["field_description"].'">
             <p>'.$action["field_description"].'</p>
                 '.$action["inputfield"].'
         </label>
     ';
+
+    $actionInformation[$key]["ruleset"]["input_requirements"] = $action["input_requirements"];
+    $actionInformation[$key]["ruleset"]["value_type_name"] = $action["value_type_name"];
+
+    if($currentCount == $numItems) {
+        $keyNumber = $currentCount + 1;
+        $actionInformation[$keyNumber]["input"]["formular"] = '<input type="submit" id="PostFormular" name="PostFormular" />';
+    } else {
+
+        $currentCount++;
+    }
+
 }
 
 header('Content-type: application/json');
